16754858c1958049e231967e82b846ffeee5b52f
IsabellaMedeiros1 <isabelladefatimamedeiros@gmail.com>
2024-11-16 10:54:38 -0300

    add parte do time

--- greenmind/src/app/page.tsx
+++ greenmind/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import { FaLeaf, FaLightbulb, FaUsers } from "react-icons/fa"; 
+import { FaLinkedin as Linkedin } from "react-icons/fa";
+import { FaGithub as Github } from "react-icons/fa";
+import { FaInstagram as Instagram } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -41,7 +44,7 @@ export default function Home() {
             O GreenMind é um projeto dedicado a criar soluções sustentáveis que promovem práticas ecológicas simples, mas eficazes. Nosso objetivo é fornecer ideias inovadoras e acessíveis para aqueles que desejam fazer a diferença no mundo. Acreditamos que pequenas ações podem ter um grande impacto na preservação do meio ambiente e na construção de um futuro mais verde e consciente.
           </p>
 
-          <div className="grid md:grid-cols-3 gap-8 mt-12">
+          <div className="grid md:grid-cols-3 gap-10 mt-12">
             <div className="flex flex-col items-center text-center">
               <div className="bg-green-600 text-white p-6 rounded-full mb-4">
                 <FaLeaf className="text-4xl" />
@@ -53,7 +56,7 @@ export default function Home() {
             </div>
             <div className="flex flex-col items-center text-center">
               <div className="bg-green-600 text-white p-6 rounded-full mb-4">
-                <FaLightbulb className="text-4xl" /> 
+                <FaLightbulb className="text-4xl" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800">Inovação</h3>
               <p className="text-gray-600 mt-2">
@@ -62,7 +65,7 @@ export default function Home() {
             </div>
             <div className="flex flex-col items-center text-center">
               <div className="bg-green-600 text-white p-6 rounded-full mb-4">
-                <FaUsers className="text-4xl" /> 
+                <FaUsers className="text-4xl" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800">Comunidade</h3>
               <p className="text-gray-600 mt-2">
@@ -72,6 +75,69 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section id="nosso-time" className="bg-white py-24">
+        <div className="max-w-7xl mx-auto px-6 text-center">
+          <h2 className="text-4xl font-bold text-gray-800 mb-6">Nosso Time</h2>
+          <p className="text-lg text-gray-600 mb-16">
+            Conheça as pessoas apaixonadas por fazer a diferença no mundo e tornar o GreenMind uma realidade.
+          </p>
+
+          <div className="grid md:grid-cols-3 gap-16 justify-center">
+            <div className="team-member bg-white rounded-xl shadow-lg p-8 text-center transition-all duration-300 transform hover:scale-105 hover:shadow-xl">
+              <Image src="/img/isabella.jpeg" alt="foto-isa" width={150} height={150} className="team-member-image rounded-full mx-auto mb-6 border-4 border-green-500" />
+              <b className="text-xl font-semibold text-gray-800">Isabella de Fátima Medeiros</b>
+              <span className="block text-gray-500 mt-2">RM 554725</span>
+              <span className="block text-gray-500 mt-1">1TDSPM</span>
+              <div className="social-icons flex justify-center gap-6 mt-6">
+                <Link href="https://github.com/IsabellaMedeiros1" target="_blank" rel="noopener noreferrer">
+                  <Github className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.instagram.com/isa.bll4x/" target="_blank" rel="noopener noreferrer">
+                  <Instagram className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.linkedin.com/in/isabella-medeiros-304769297" target="_blank" rel="noopener noreferrer">
+                  <Linkedin className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+              </div>
+            </div>
+            <div className="team-member bg-white rounded-xl shadow-lg p-8 text-center transition-all duration-300 transform hover:scale-105 hover:shadow-xl">
+              <Image src="/img/eduardo-m.jpeg" alt="foto-dudao" width={150} height={150} className="team-member-image rounded-full mx-auto mb-6 border-4 border-green-500" />
+              <b className="text-xl font-semibold text-gray-800">Eduardo Miguel Forato Monteiro</b>
+              <span className="block text-gray-500 mt-2">RM 555871</span>
+              <span className="block text-gray-500 mt-1">1TDSPM</span>
+              <div className="social-icons flex justify-center gap-6 mt-6">
+                <Link href="https://github.com/EduardoMiguelFM" target="_blank" rel="noopener noreferrer">
+                  <Github className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.instagram.com/dudao_miguelfm/" target="_blank" rel="noopener noreferrer">
+                  <Instagram className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.linkedin.com/in/eduardo-miguel-forato-monteiro-10b9822b7" target="_blank" rel="noopener noreferrer">
+                  <Linkedin className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+              </div>
+            </div>
+            <div className="team-member bg-white rounded-xl shadow-lg p-8 text-center transition-all duration-300 transform hover:scale-105 hover:shadow-xl">
+              <Image src="/img/cicero.jpeg" alt="foto-cicero" width={150} height={150} className="team-member-image rounded-full mx-auto mb-6 border-4 border-green-500" />
+              <b className="text-xl font-semibold text-gray-800">Cicero Gabriel Oliveira Serafim</b>
+              <span className="block text-gray-500 mt-2">RM 556996</span>
+              <span className="block text-gray-500 mt-1">1TDSPM</span>
+              <div className="social-icons flex justify-center gap-6 mt-6">
+                <Link href="https://github.com/ciceroserafim" target="_blank" rel="noopener noreferrer">
+                  <Github className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.instagram.com/cicero__serafim/" target="_blank" rel="noopener noreferrer">
+                  <Instagram className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+                <Link href="https://www.linkedin.com/in/c%C3%ADcero-gabriel-oliveira-serafim-953b0b20b/" target="_blank" rel="noopener noreferrer">
+                  <Linkedin className="text-gray-600 hover:text-green-600 transition-colors duration-200" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </>
   );
 }
