66170c43257ecffe07af79f5a74826c11c1a9d12
IsabellaMedeiros1 <isabelladefatimamedeiros@gmail.com>
2024-11-22 21:20:11 -0300

    alsasldk

--- greenmind/src/app/types.ts
+++ greenmind/src/app/types.ts
@@ -7,61 +7,49 @@ export const imagensCategoria: Record<string, string[]> = {
     "Alimentação Sustentável": [
       "/img/alimentacao1.jpg",
       "/img/alimentacao2.jpg",
-      "/img/alimentacao3.jpg",
     ],
     "Construção": [
         "/img/construcao1.jpg",
         "/img/construcao2.jpg",
-        "/img/construcao3.jpg",
     ],
     "Educação": [
         "/img/educacao1.jpg",
         "/img/educacao2.jpg",
-        "/img/educacao3.jpg",
     ],
     "Energia": [
         "/img/energia1.jpg",
-        "/img/energia1.jpg",
-        "/img/energia1.jpg",
+        "/img/energia2.jpg",
     ],
     "Hortas": [
         "/img/hortas1.jpg",
         "/img/hortas2.jpg",
-        "/img/hortas3.jpg",
     ],
     "Natureza": [
         "/img/natureza1.jpg",
         "/img/natureza2.jpg",
-        "/img/natureza3.jpg",
     ],
     "Proteção Animal": [
         "/img/animal1.jpg",
         "/img/animal2.jpg",
-        "/img/animal3.jpg",
     ],
     "Reciclagem": [
         "/img/reciclagem1.jpg",
         "/img/reciclagem2.jpg",
-        "/img/reciclagem3.jpg",
     ],
     "Resíduos": [
         "/img/residuos1.jpg",
         "/img/residuos2.jpg",
-        "/img/residuos3.jpg",
     ],
     "Tecnologia": [
         "/img/tecnologia1.jpg",
         "/img/tecnologia2.jpg",
-        "/img/tecnologia3.jpg",
     ],
     "Transportes": [
         "/img/transporte1.jpg",
         "/img/transporte2.jpg",
-        "/img/transporte.jpg",
     ],
     "Água": [
-        "/img/agua.jpg",
-        "/img/agua.jpg",
-        "/img/agua.jpg",
+        "/img/agua1.jpg",
+        "/img/agua2.jpg",
     ],
   };
